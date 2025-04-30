fd3f3b2b83c3ffdc97cc62c582c84429f14fe42b
Gabriel Prata <ghprata7245@gmail.com>
2025-04-30 10:08:22 -0300

    Projeto criado

---
+++ src/test/java/com/prata/cursoJava/SpringwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prata.cursoJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
