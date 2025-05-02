2cfddd0752ca890a2d34a1b64c6c2bc49cb40328
GabrielPraata <ghprata7245@gmail.com>
2025-05-02 09:05:28 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/prata/cursoJava/config/TestConfig.java
+++ src/main/java/com/prata/cursoJava/config/TestConfig.java
@@ -44,7 +44,7 @@ public class TestConfig implements CommandLineRunner {
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        User u2 = new User(null, "Gabriel Prata", "alex@gmail.com", "977777777", "123456");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
